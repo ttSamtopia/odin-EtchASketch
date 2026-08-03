8f758f155a05372cec4d4000f0c3733f8fac0b57
ttSamtopia <s-linden@live.se>
2026-08-03 21:16:29 +0200

    Improve color logic

--- script.js
+++ script.js
@@ -7,14 +7,12 @@ for (let row = 0; row < 16; row++) {
     for (let column = 0; column < 16; column++) {
         let square = document.createElement("div");
         square.classList.add("square")
-        square.style.backgroundColor = "rgb(253, 68, 68, 0)";
+        let opacity = 0;
+        square.style.backgroundColor = `rgb(253, 68, 68, ${opacity})`;
 
         square.addEventListener("mouseenter", (e) => {
-            let currentColorArray = square.style.backgroundColor.split(",");
-            let currentOpacity = parseFloat(currentColorArray[3]);
-            if (currentOpacity < 1) currentOpacity += 0.1;
-            currentColorArray[3] = currentOpacity + ")";
-            square.style.backgroundColor = currentColorArray.join(",");
+            if (opacity < 1) opacity += 0.1;
+            square.style.backgroundColor = `rgb(253, 68, 68, ${opacity})`;
         });
         
         rowDiv.appendChild(square);
