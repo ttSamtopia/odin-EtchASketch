3b191fbef78a012f00c1121bfd54e707bc367f5b
ttSamtopia <s-linden@live.se>
2026-08-03 21:33:24 +0200

    Update grid creation to function

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-const container = document.querySelector("#container");
+const CONTAINER = document.querySelector("#container");
 
-for (let row = 0; row < 16; row++) {
-    let rowDiv = document.createElement("div");
-    rowDiv.classList.add("row");
+function createGrid (gridSize = 16) {
+    for (let row = 0; row < gridSize; row++) {
+        let rowDiv = document.createElement("div");
+        rowDiv.classList.add("row");
 
-    for (let column = 0; column < 16; column++) {
-        let square = document.createElement("div");
-        square.classList.add("square")
-        let opacity = 0;
-        square.style.backgroundColor = `rgb(253, 68, 68, ${opacity})`;
-
-        square.addEventListener("mouseenter", (e) => {
-            if (opacity < 1) opacity += 0.1;
+        for (let column = 0; column < gridSize; column++) {
+            let square = document.createElement("div");
+            square.classList.add("square")
+            let opacity = 0;
             square.style.backgroundColor = `rgb(253, 68, 68, ${opacity})`;
-        });
-        
-        rowDiv.appendChild(square);
-    }
 
-    container.appendChild(rowDiv);
+            square.addEventListener("mouseenter", (e) => {
+                if (opacity < 1) opacity += 0.1;
+                square.style.backgroundColor = `rgb(253, 68, 68, ${opacity})`;
+            });
+            
+            rowDiv.appendChild(square);
+        }
+
+        CONTAINER.appendChild(rowDiv);
+    }
 }
+
+createGrid()
